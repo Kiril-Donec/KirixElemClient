ef2ebb51f153b2e73f2659e0a1f1c03f447ed7fc
Кіріл Донець <kiril@MacBook-Air-Kiril.local>
2025-02-01 01:17:47 +0200

    Обнова

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Включаем минификацию
+  config.optimization = {
+    ...config.optimization,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  };
+
+  return config;
+};
